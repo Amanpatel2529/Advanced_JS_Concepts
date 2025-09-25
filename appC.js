400f61cfa7837253648c63fb2e6e76d2c24b875d
Aman Patel <amanpatel.ajp@gmail.com>
2025-09-25 13:12:53 +0530

    common js vs Es6 module

---
+++ appC.js
@@ -0,0 +1,7 @@
+const mathOperations = require("./mathOperationsC.js");
+
+console.log("Addition is", mathOperations.add(2,4));
+console.log("Subtraction is", mathOperations.subtract(8,4));
+console.log("Multiplication is", mathOperations.multiply(2,4));
+
+
