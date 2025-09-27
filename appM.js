5032b6f48b7d6170e53b36dec8e200dcf80f0ca3
Aman Patel <amanpatel.ajp@gmail.com>
2025-09-27 20:02:39 +0530

    module js and common js

---
+++ appM.js
@@ -0,0 +1,15 @@
+//Module import file
+
+//default import 
+import multiply from "./mathOperationsM.js";
+
+//named import
+import {add, subtract} from "./mathOperationsM.js"
+
+console.log(multiply(2,3));
+console.log(add(2,3));
+console.log(subtract(6,3));
+
+
+
+
